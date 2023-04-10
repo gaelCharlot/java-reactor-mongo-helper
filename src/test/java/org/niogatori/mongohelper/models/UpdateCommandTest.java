be91bf059b438290cc6b29ffd1a4d48d03f2dc36
Niogatori <55828462+gaelCharlot@users.noreply.github.com>
2023-04-10 17:03:18 +0200

    Release 1.0.0 (#16)
    * feat(RMH-01): v1 of a mongobulkwriter to bulk in 1 connection an update with pipeline (#2)
    
    * Feat(RMH-02): reorganize repository (#3)
    
    * Feat(rmh-02) : add github worklow (#4)
    
    * feat(RMH-04): add CI (#5)
    
    * feat(RMH-04): add CI
    
    * feat(RMH-04): ci
    
    * Feat(rmh-04): wording (#6)
    
    * feat(RMH-04): add CI
    
    * feat(RMH-04): ci
    
    * feat(RMH-04): wording
    
    * Wording/rmh 05 (#7)
    
    * wording(RMH-05): rename application.properties
    
    * wording(RMH-05): rename application.properties
    
    * test/RMH-06: fix embedded mongodb test (#8)
    
    * doc(RMH-07): add documentation and generate JavaDoc(#9)
    
    * doc(RMH-07): add documentation in README and TECHNO
    
    * doc(RMH-07): add documention about features of this library
    
    * doc(RMH-07): link javadoc
    
    * Doc(rmh-07) : update documentation into README (#10)
    
    * fix(Javadoc): move javadoc/  to root to deploy in github page (#12)
    
    * fix(Javadoc): move javadoc/  to root to deploy in github page
    
    * fix(Javadoc): fix link
    
    * fix(Javadoc): fix link
    
    * feat(update-cmd-optimization) : split convertor and get update command in a new class (#13)
    
    * feat(update-cmd-optimization) : split convertor and get update command in new class
    
    * feat(update-cmd-optimization): add TU
    
    * feat(upgrade-version): up to v1.0.0 (#14)

---
+++ src/test/java/org/niogatori/mongohelper/models/UpdateCommandTest.java
@@ -0,0 +1,128 @@
+package org.niogatori.mongohelper.models;
+
+import org.bson.BsonDocument;
+import org.bson.Document;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UpdateCommandTest {
+
+    private final static Clock CLOCK =
+            Clock.fixed(LocalDateTime.of(2023, 4, 10, 12, 0, 0).toInstant(ZoneOffset.UTC), ZoneOffset.UTC);
+
+    @Nested
+    class BsonConvertorTest {
+        @Test
+        void shouldConvertUpdateCommandToBson() {
+            Document update1 = new Document("q", new Document("_id", "myId"))
+                    .append("u", new Document("field", "myNewValue"))
+                    .append("upsert", true)
+                    .append("multi", false);
+            List<Document> updates = List.of(update1);
+            UpdateCommand updateCommand = new UpdateCommand("MyCollection", updates, true, null, false, null, null);
+
+            assertThat(updateCommand.asBson()).satisfies(command -> {
+                assertThat(command)
+                        .hasFieldOrPropertyWithValue("update", "MyCollection")
+                        .hasFieldOrPropertyWithValue("updates", List.of(update1))
+                        .hasFieldOrPropertyWithValue("ordered", true)
+                        .hasFieldOrPropertyWithValue("bypassDocumentValidation", false)
+                        .hasFieldOrPropertyWithValue("let", null)
+                        .hasFieldOrPropertyWithValue("comment", null);
+                assertThat(command)
+                        .doesNotContainKey("writeConcern");
+            });
+        }
+    }
+
+    @Nested
+    class WithUpdatesTest {
+        @Test
+        void shouldAddUpdatesQuery_WithOneQueryAndFieldsToSet() {
+            Document updateQuery = new Document("q", new Document("_id", "myId"))
+                    .append("u", new Document("$set", new Document("field", "myNewValue")).toBsonDocument())
+                    .append("upsert", true)
+                    .append("multi", false);
+
+            UpdateCommand updateCommand = new UpdateCommand("MyCollection", null, true, null, false, null, null);
+
+            assertThat(updateCommand)
+                    .hasFieldOrPropertyWithValue("update", "MyCollection")
+                    .hasFieldOrPropertyWithValue("updates", null)
+                    .hasFieldOrPropertyWithValue("ordered", true)
+                    .hasFieldOrPropertyWithValue("bypassDocumentValidation", false)
+                    .hasFieldOrPropertyWithValue("comment", null)
+                    .hasFieldOrPropertyWithValue("let", null);
+
+            assertThat(updateCommand.withUpdates(new Document("_id", "myId"), new Document("field", "myNewValue"), null,
+                    null, true, false))
+                            .isEqualTo(new UpdateCommand("MyCollection", List.of(updateQuery), true, null, false, null,
+                                    null));
+
+        }
+
+        @Test
+        void shouldAddUpdatesQuery_WithManyQueriesAndFieldsToSet() {
+            Document updateQuery = new Document("q", new Document("_id", "myId"))
+                    .append("u", new Document("$set", new Document("field", "myNewValue")).toBsonDocument())
+                    .append("upsert", true)
+                    .append("multi", false);
+
+            UpdateCommand updateCommand = new UpdateCommand("MyCollection", null, true, null, false, null, null);
+
+            assertThat(updateCommand)
+                    .hasFieldOrPropertyWithValue("update", "MyCollection")
+                    .hasFieldOrPropertyWithValue("updates", null)
+                    .hasFieldOrPropertyWithValue("ordered", true)
+                    .hasFieldOrPropertyWithValue("bypassDocumentValidation", false)
+                    .hasFieldOrPropertyWithValue("comment", null)
+                    .hasFieldOrPropertyWithValue("let", null);
+
+            assertThat(updateCommand.withUpdates(
+                    Map.of(new Document("_id", "myId"), Optional.of(new Document("field", "myNewValue"))), null,
+                    null, true, false))
+                            .isEqualTo(new UpdateCommand("MyCollection", List.of(updateQuery), true, null, false, null,
+                                    null));
+        }
+
+        @Test
+        void shouldAddUpdatesQuery_WithFieldsToUnsetAndFieldsToSetOnInsert() {
+            UpdateCommand updateCommand = new UpdateCommand("MyCollection", null, true, null, false, null, null);
+
+            assertThat(updateCommand)
+                    .hasFieldOrPropertyWithValue("update", "MyCollection")
+                    .hasFieldOrPropertyWithValue("updates", null)
+                    .hasFieldOrPropertyWithValue("ordered", true)
+                    .hasFieldOrPropertyWithValue("bypassDocumentValidation", false)
+                    .hasFieldOrPropertyWithValue("comment", null)
+                    .hasFieldOrPropertyWithValue("let", null);
+
+            assertThat(updateCommand.withUpdates(
+                    Map.of(new Document("_id", "myId"), Optional.of(new Document("field", "myNewValue"))),
+                    List.of("label", "metadata"),
+                    new Document("creationDate", LocalDate.now(CLOCK)), true, false))
+                            .hasFieldOrPropertyWithValue("update", "MyCollection")
+                            .hasFieldOrPropertyWithValue("ordered", true)
+                            .hasFieldOrPropertyWithValue("bypassDocumentValidation", false)
+                            .hasFieldOrPropertyWithValue("comment", null)
+                            .hasFieldOrPropertyWithValue("let", null)
+                            .satisfies(cmd -> assertThat(cmd.getUpdates()).hasSize(1)
+                                    .allMatch(document -> {
+                                        BsonDocument u = document.get("u", BsonDocument.class);
+                                        return u.containsKey("$set") && u.containsKey("$unset")
+                                                && u.containsKey("$setOnInsert");
+                                    }));
+        }
+    }
+
+}
